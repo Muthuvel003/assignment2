53f58819f6cdfc590e36638fc270c338994c0b48
Muthuvel003 <m.muthuvel1081@gmail.com>
2016-07-14 20:01:42 +0530

    assign2

--- includes/js/script.js
+++ includes/js/script.js
@@ -8,3 +8,14 @@ Updated: August 2013
 Notes:	 Hand coded for Udemy.com
 
 */
+$('document').ready(function() {
+    $('.navbar-text').each(function() {
+        var target = $(this).html();
+        target = target.split("");
+        var result = "";
+        for (var i = 0, len = target.length; i < len; i++) {
+            result += '<span class="' + target[i] + '">' + target[i] + '</span>';
+        }
+        $(this).html(result);
+    });
+});
